bc838314292d0942573c59f0c2ba0c55ede8e3e2
Lorenz0velez <lorenzoveleznavarro@gmail.com>
2023-08-21 14:26:56 -0300

    añadiendo ruta backend railway

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from './views/Home';
 import axios from 'axios';
-axios.defaults.baseURL = "http://localhost:3001/" ;
+// axios.defaults.baseURL = "http://localhost:3001/" ;
+axios.defaults.baseURL = "https://vineart-backend-production.up.railway.app/" ;
 function App() {
   return (
     <div className="App">
